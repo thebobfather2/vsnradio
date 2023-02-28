e3cdd64765e79719f5859d4200cef00cfe715d54
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-02-28 00:50:16 -0500

    got player button to go to marketplace based on what is in player

---
+++ songsdata.js
@@ -0,0 +1,102 @@
+exports.songsdata = [
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+    {
+        "title": "Million Dollar Smile",
+        "url": "https://wallet.preview.contentfabric.io/?preview=iq__PoyQD1L385EWHguKYvGNUbpyXbP&mid=iq__PoyQD1L385EWHguKYvGNUbpyXbP&hgm=/#/marketplace/iq__PoyQD1L385EWHguKYvGNUbpyXbP/store/Gr1RJ76W34721tpr9n1TxC",
+        "hash": "hq__BSki1joYvZ5pxc4PAWr2zWxT8A681Vmx3xzTrihC66R5sZMJKzAzC2mA7Wnfc83sdg4r6XcKo7"
+    },
+]
